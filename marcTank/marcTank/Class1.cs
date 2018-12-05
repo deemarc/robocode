f336999a258e390a3ee4f8db3c6db5e7ab14a73c
deemarc <marteleus@gmail.com>
2018-12-05 16:07:55 +0700

    randomTrackFire Done

--- marcTank/marcTank/Class1.cs
+++ marcTank/marcTank/Class1.cs
@@ -10,19 +10,61 @@ namespace marcTank
 {
     public class Class1 : AdvancedRobot
     {
+        //private int scanDirection = 1;
+        private EnemyBot enemy = new EnemyBot();
+        Random rndmizer = new Random();
         public override void Run()
         {
+            this.IsAdjustRadarForRobotTurn = true;
+            this.IsAdjustGunForRobotTurn = true;
+            enemy.Reset();
             while(true)
             {
-                TurnGunRight(360);
+                //TurnGunRight(360);
+                Move();
+                TurnRadarRight(360);
+                
+
             }
         }
-        
+        public override void OnRobotDeath(RobotDeathEvent evnt)
+        {
+            if (evnt.Name.Equals(enemy.GetName()))
+            {
+                enemy.Reset();
+            }
+        }
+
         //This will be called when an enemy is detected, you can access enemy's attributes throught evnt eg. evnt.Name
         public override void OnScannedRobot(ScannedRobotEvent evnt)
         {
-            TurnRight(evnt.Bearing);
-            Fire(1);
+            double firePwr = 0.0;
+            if(//we have no enemy or
+                enemy.GetName() == "" ||
+                //the one we just spotted is closer 0r
+                enemy.GetDistance() > evnt.Distance ||
+                //its the same enemy
+                enemy.GetName() == evnt.Name)
+            {
+                enemy.Updata(evnt);
+                //scanDirection *= -1;
+                TurnRight(evnt.Bearing);
+                //SetTurnRadarRight(scanDirection * 360);
+                SetTurnRadarRight(this.Heading - this.RadarHeading);
+                SetTurnGunRight(this.Heading - this.GunHeading);
+                firePwr = 400 / enemy.GetDistance();
+                Fire(firePwr);
+            }
+   
+            
+            
+        }
+
+        void Move()
+        {
+            int direction = rndmizer.Next(0, 360);
+            TurnRight(direction);
+            Ahead(rndmizer.Next(50, 200));
         }
     }
 }
---
+++ marcTank/marcTank/EnemyBot.cs
@@ -0,0 +1,64 @@
+﻿using Robocode;
+namespace marcTank
+{
+    internal class EnemyBot 
+    {
+        private double bearing = 0.0;
+        private double distance = 0.0;
+        private double energy = 0.0;
+        private double heading = 0.0;
+        private double velocity = 0.0;
+
+        private string name = "";
+
+        public EnemyBot()
+        {
+            this.Reset();
+        }
+        public double GetBearing()
+        {
+            return this.bearing;
+        }
+        public double GetDistance()
+        {
+            return this.distance;
+        }
+        public double GetEnergy()
+        {
+            return this.energy;
+        }
+        public double GetHeading()
+        {
+            return this.heading;
+        }
+        public double GetVelociy()
+        {
+            return this.velocity;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+        public void Updata(ScannedRobotEvent evnt)
+        {
+            bearing     = evnt.Bearing;
+            distance    = evnt.Distance;
+            energy      = evnt.Energy;
+            heading     = evnt.Heading;
+            velocity    = evnt.Velocity;
+
+            name = evnt.Name;
+        }
+        public void Reset()
+        {
+            bearing = 0.0;
+            distance = 0.0;
+            energy = 0.0;
+            heading = 0.0;
+            velocity = 0.0;
+
+            name = "";
+        }
+    }
+}
